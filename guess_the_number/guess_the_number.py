aa7abf1fafff4291ac2e2451b676a8415ec1e290
Paramjot Singh <param.chhina@gmail.com>
2019-06-05 05:15:10 -0500

    added reset and quit buttons, rescaled row numbers for flexibility

--- guess_the_number/guess_the_number.py
+++ guess_the_number/guess_the_number.py
@@ -17,6 +17,7 @@ def new_game():
     ntry = 7
     msg = "You have {0} trials remaining".format(ntry)
     trials.set(msg)
+    entry_question.config(state = 'normal')
 
 def set_message(event):
     """Updates the message with new text in entry field."""
@@ -58,20 +59,31 @@ label_title = tk.Label(root, text = "Guess the Number",
         font = ("", 16))
 label_title.grid(row = 0, column = 0, columnspan = 2)
 
+reset_button = tk.Button(root, text = "Reset", command = new_game,
+        bg = "#009300", fg = "#e6e6e6")
+reset_button.grid(row = 60, column = 0, sticky = tk.W,
+        padx = 5, pady = 5)
+
+quit_button = tk.Button(root, text = "Quit", command = root.destroy,
+        bg = "#ff4848", fg = "#e6e6e6")
+quit_button.grid(row = 60, column = 1, sticky = tk.E,
+        padx = 5, pady = 5)
+
+
 label_question = tk.Label(root, text = "Enter your guess")
-label_question.grid(row = 1, column = 0, padx = 10, pady = 20)
+label_question.grid(row = 10, column = 0, padx = 10, pady = 20)
 
 entry_question = tk.Entry(root)
-entry_question.grid(row = 1, column = 1, padx = 10)
+entry_question.grid(row = 10, column = 1, padx = 10)
 
 label_guess = tk.Label(root, textvariable = var)
-label_guess.grid(row = 3, column = 0, pady = 5, columnspan = 2)
+label_guess.grid(row = 30, column = 0, pady = 5, columnspan = 2)
 
 label_result = tk.Label(root, textvariable = result)
-label_result.grid(row = 4, column = 0, pady = 5, columnspan = 2)
+label_result.grid(row = 40, column = 0, pady = 5, columnspan = 2)
 
 label_trials = tk.Label(root, textvariable = trials)
-label_trials.grid(row = 5, column = 0, pady = 5, columnspan = 2)
+label_trials.grid(row = 50, column = 0, pady = 5, columnspan = 2)
 
 # add = "+" adds the handler to handler list
 # this allows multiple handlers to bind to single event
