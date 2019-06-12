4196b9446a40a6d6ec4ea549da0e7cc415fc5b71
Paramjot Singh <param.chhina@gmail.com>
2019-06-12 04:30:07 -0500

    added score

--- stopwatch/stopwatch.py
+++ stopwatch/stopwatch.py
@@ -4,7 +4,7 @@ from tkinter import font
 import threading
 
 root = tk.Tk()
-root.geometry("450x350")
+root.geometry("450x400")
 root.title("Stopwatch")
 
 # ---- Define globals ---
@@ -12,6 +12,9 @@ is_running = False
 start = 0
 counter = tk.StringVar()
 fontstyle = font.Font(size = 24)
+score = 0
+ntry = 0
+score_display = tk.StringVar()
 
 
 # ---- Define helper functions ----
@@ -27,6 +30,9 @@ def format_start(start):
     formatted_time = "{0}:{1}{2}".format(minutes, tens_place, seconds)
     return formatted_time
     
+def format_score():
+    score_disp = f"Score: {score}/{ntry}"
+    return score_disp
 
 def timer():
     global start, counter
@@ -37,14 +43,19 @@ def timer():
         start = round(start, 1)
 
 def timer_reset():
-    global is_running, start, counter
+    global is_running, start, counter, ntry, score
     is_running = False
-    start = 0
+    start = ntry = score = 0
     counter.set(format_start(start))
+    score_display.set(format_score())
 
 def timer_stop():
-    global is_running
+    global is_running, ntry, score
     is_running = False
+    ntry += 1
+    if int(start * 10) % 10 == 0:
+        score += 1
+    score_display.set(format_score())
 
 def timer_start():
     global is_running
@@ -69,6 +80,10 @@ reset_button.grid(row = 0, column = 2, pady = 25)
 counter_label = tk.Label(root, textvariable = counter, bg = "#2c302e",
         fg = "#f7f6dc", font = ("default", 60), height = 2, width = 8)
 counter_label.grid(row = 1, columnspan = 3, pady = 20, padx = 20)
+
+score_label = tk.Label(root, textvariable = score_display, fg = "#2c302e",
+        font = ("default", 20))
+score_label.grid(row = 2, column = 2, pady = 10, padx = 10)
 
 timer_reset()
 
