46002d31ddba2568b3f9d125a41ca8a9009cdb53
Paramjot Singh <param.chhina@gmail.com>
2019-06-26 19:14:23 -0500

    added left and right paddles that can be moved with f, d, j and k keys

--- pong/pong.py
+++ pong/pong.py
@@ -5,25 +5,29 @@ root = tk.Tk()
 root.title("Pong")
 
 # First foray into classes
-class Paddle:
-    """paddle for the pong game"""
-    WIDTH = 20
-    HEIGHT = 90
-
-    def __init__(self, canvas, x0, y0, color):
-        self.paddle = canvas.create_rectangle(x0, y0, x0 + Paddle.WIDTH,
-                y0 + Paddle.HEIGHT, fill = color)
-
 class Pong:
     def __init__(self, master):
         self.width = 1000
         self.height = 800
         self.window = tk.Canvas(master, width = self.width, height = self.height)
         self.window.grid()
+        self.paddlewidth = 20
+        self.paddleheight = 100
+        self.left_padpos = 0
+        self.right_padpos = 980
 
         # draw paddles
-        left_paddle = Paddle(self.window, 0, 0, "red")
-        right_paddle = Paddle(self.window, self.width - Paddle.WIDTH, 0, "blue")
+        self.left_paddle = self.window.create_rectangle(self.left_padpos,
+                self.left_padpos,
+                self.left_padpos + self.paddlewidth,
+                self.left_padpos + self.paddleheight,
+                fill = "red")
+        self.right_paddle = self.window.create_rectangle(self.right_padpos,
+                0,
+                self.right_padpos + self.paddlewidth,
+                self.paddleheight,
+                fill = "blue")
+
 
         # draw ball at the center of the canvas
         center = [self.width / 2, self.height / 2]
@@ -36,13 +40,13 @@ class Pong:
 
         # initialize velocity
         self.v = [1, 1]
+
         
     def velocity(self, pos):
         if pos[2] > self.width or pos[0] < 0:
             self.v[0] *= -1
         if pos[3] > self.height or pos[1] < 0:
             self.v[1] *= -1
-        print(self.v)
         return self.v
 
     def move_ball(self):
@@ -50,10 +54,22 @@ class Pong:
             pos = self.window.coords(self.ball)
             ball_speed = self.velocity(pos)
             self.window.move(self.ball, ball_speed[0], ball_speed[1])
-            print(pos)
             time.sleep(0.02)
             root.update()
 
-pong = Pong(root)
+    def move_paddle(self, event):
+        pos_left = self.window.coords(self.left_paddle)
+        pos_right = self.window.coords(self.right_paddle)
+        if event.char == "f" and pos_left[3] < self.height:
+            self.window.move(self.left_paddle, 0, 10)
+        if event.char == "d" and pos_left[1] > 0:
+            self.window.move(self.left_paddle, 0, -10)
+        if event.char == "j" and pos_right[3] < self.height:
+            self.window.move(self.right_paddle, 0, 10)
+        if event.char == "k" and pos_right[1] > 0:
+            self.window.move(self.right_paddle, 0, -10)
+            root.update()
 
+pong = Pong(root)
+root.bind("<Key>", pong.move_paddle)
 pong.move_ball()
